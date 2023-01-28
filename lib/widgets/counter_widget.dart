fa18585a842ada5965e2b44daca9846042546b2c
Kuzetov-Alexandr <107497862+Kuzetov-Alexandr@users.noreply.github.com>
2023-01-28 14:28:51 +0500

    28.01.2023

--- lib/widgets/counter_widget.dart
+++ lib/widgets/counter_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sizer/sizer.dart';
+import 'package:tasbix/main.dart';
 
 class CounterWidget extends StatefulWidget {
   const CounterWidget({
@@ -25,15 +26,25 @@ class _CounterWidgetState extends State<CounterWidget> {
         ),
         color: Color(0xffFFFFFF),
       ),
-      width: 100.w,
+      width: mapperOrientation(portrait: 91.w, landscape: 208.w),
       height: 25.h,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           TextButton(
             style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all(Size(35, 4.h)),
-              maximumSize: MaterialStateProperty.all(Size(35, 4.h)),
+              minimumSize: MaterialStateProperty.all(
+                mapperOrientation(
+                  portrait: Size(35, 4.h),
+                  landscape: Size(13.w, 13.w),
+                ),
+              ),
+              maximumSize: MaterialStateProperty.all(
+                mapperOrientation(
+                  portrait: Size(35, 4.h),
+                  landscape: Size(13.w, 13.w),
+                ),
+              ),
               shape: MaterialStateProperty.all(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -51,42 +62,60 @@ class _CounterWidgetState extends State<CounterWidget> {
               'assets/images/remove.svg',
             ),
           ),
-          Container(
-            width: 39.w,
-            height: 18.h,
-            decoration: const ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
+          InkWell(
+            onTap: () {
+              setState(() {
+                counter++;
+              });
+            },
+            child: Container(
+              width: mapperOrientation(portrait: 39.w, landscape: 60.w),
+              // 39.w,
+              height: 18.h,
+              decoration: const ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
                 ),
+                color: Color(0xff4664FF),
               ),
-              color: Color(0xff4664FF),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '$counter',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 48,
-                      color: Colors.white),
-                ),
-                const Text(
-                  'Dhikr',
-                  // style: GoogleFonts.gilroy,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      color: Colors.white),
-                ),
-              ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$counter',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 48,
+                        color: Colors.white),
+                  ),
+                  const Text(
+                    'Dhikr',
+                    // style: GoogleFonts.gilroy,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
             ),
           ),
           TextButton(
             style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all(Size(35, 4.h)),
-              maximumSize: MaterialStateProperty.all(Size(35, 4.h)),
+              minimumSize: MaterialStateProperty.all(
+                mapperOrientation(
+                  portrait: Size(35, 4.h),
+                  landscape: Size(13.w, 13.w),
+                ),
+              ),
+              maximumSize: MaterialStateProperty.all(
+                mapperOrientation(
+                  portrait: Size(35, 4.h),
+                  landscape: Size(13.w, 13.w),
+                ),
+              ),
               shape: MaterialStateProperty.all(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
--- lib/widgets/small_container.dart
+++ lib/widgets/small_container.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sizer/sizer.dart';
+import 'package:tasbix/main.dart';
 
-class SmallContainer extends StatelessWidget {
+class SmallContainer extends StatefulWidget {
   const SmallContainer({Key? key, required this.number}) : super(key: key);
 
   final int number;
 
+  @override
+  State<SmallContainer> createState() => _SmallContainerState();
+}
+
+class _SmallContainerState extends State<SmallContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,15 +24,16 @@ class SmallContainer extends StatelessWidget {
         ),
         color: Color(0xffF9F9F9),
       ),
-      width: 90.w,
+      width: mapperOrientation(portrait: 90.w, landscape: 150.w),
       height: 6.h,
       child: Row(
         children: [
-          const SizedBox(width: 20),
+          SizedBox(width: 5.5.w),
           SizedBox(
-            width: 25,
+            width: 7.w,
             child: Text(
-              '$number',
+              '${widget.number}',
+              maxLines: 3,
               style: const TextStyle(
                   fontWeight: FontWeight.w700,
                   fontSize: 20,
@@ -34,35 +41,35 @@ class SmallContainer extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
-          const SizedBox(width: 30),
-          const Text(
-            'Name of the file Dhikr',
-            style: TextStyle(
-                fontWeight: FontWeight.w400, fontSize: 12, color: Colors.black),
+          SizedBox(width: 8.w),
+          SizedBox(
+            width: mapperOrientation(portrait: 30.w, landscape: 97.w),
+            child: const Text(
+              'Name of the file Dhikr',
+              maxLines: 3,
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Colors.black),
+            ),
           ),
-          const SizedBox(width: 10),
+          SizedBox(width: 2.5.w),
           const Text(
             '22.01.2023',
             style: TextStyle(
                 fontWeight: FontWeight.w500, fontSize: 10, color: Colors.black),
             //  Color(0xff9F9F9F)),
           ),
-          const SizedBox(width: 10),
+          SizedBox(width: 2.5.w),
           IconButton(
-            iconSize: 30,
             enableFeedback: true,
-
             focusColor: Colors.green,
             splashColor: Colors.green,
-            // style: ButtonStyle(
-            //   fixedSize:
-            //       MaterialStateProperty.all(Size(20, 15)),
-            // ),
             onPressed: () {},
             icon: SvgPicture.asset(
               'assets/images/setting.svg',
-              width: 20,
-              height: 5,
+              width: 5.w,
+              height: 1.5.w,
               fit: BoxFit.fill,
             ),
           ),
